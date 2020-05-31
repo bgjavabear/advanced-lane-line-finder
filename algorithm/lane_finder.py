a3d86670659031d1c271d29b4b986e97730cb34e
Alexander Kiselev <agvarelex@gmail.com>
2020-05-31 17:53:02 +0300

    video

---
+++ algorithm/lane_finder.py
@@ -0,0 +1,50 @@
+import cv2
+import glob
+import numpy as np
+
+from image_processing.image_processing import find_line_edges
+from image_processing.calibration import calibrate_camera
+from image_processing.transform import perspective_transform, reverse_transform
+from measurement.curvature import measure_relative_position, measure_curvature_meters, get_px_to_meters_coef
+from image_processing.sliding_window import fit_polynomial_meters, fit_polynomial
+
+
+def pipeline(img):
+    # Calibration
+    chessboard_images = glob.glob('data/main/camera_cal/*.jpg')
+    nx = 9  # chessboard corners in x direction
+    ny = 6  # chessboard corners in y direction
+
+    calibration_result = calibrate_camera(chessboard_images, (nx, ny))
+    ret = calibration_result["ret"]
+    mtx = calibration_result["mtx"]
+    dist = calibration_result["dist"]
+    rvecs = calibration_result["rvecs"]
+    tvecs = calibration_result["tvecs"]
+
+    # Apply a distortion correction to raw images
+    undistorted_img = cv2.undistort(img, mtx, dist, None, mtx)
+    line_edges = find_line_edges(undistorted_img, yellow_thresh=(130, 255),
+                                 white_thresh=((190, 190, 190), (255, 255, 255)),
+                                 gradient_yellow_threshold=(20, 255), gradient_white_thresh=(20, 255))
+
+    # Bird-view transform
+    warped, M, Minv, src, dst = perspective_transform(line_edges)
+
+    # sliding window algorithm
+    left_fitx, right_fitx, ploty, out_img = fit_polynomial(warped)
+
+    # Calculate curvature and relative position of a car
+    ym_per_pix, xm_per_pix = get_px_to_meters_coef(warped.shape)
+    left_fit, right_fit = fit_polynomial_meters(warped, ym_per_pix, xm_per_pix)
+    left_curverad, right_curverad = measure_curvature_meters(ploty, left_fit, right_fit, ym_per_pix)
+    R = (left_curverad + right_curverad) / 2
+    D = measure_relative_position(warped, xm_per_pix)
+
+    # reverse warp
+    output = reverse_transform(undistorted_img, img, warped, Minv, left_fitx, right_fitx, ploty)
+    cv2.putText(output,
+                f'Radius of Curvature = {R:.2f}(m).Vehicle is {np.absolute(D):.2f}m {"left" if D < 0 else "right"} of '
+                f'center',
+                (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+    return output
--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-import glob
 import cv2
 import os
-import numpy as np
-from image_processing.calibration import calibrate_camera
-from image_processing.image_processing import find_line_edges
-from image_processing.transform import perspective_transform, reverse_transform
-from image_processing.sliding_window import fit_polynomial, fit_polynomial_meters
-from measurement.curvature import measure_curvature_meters, get_px_to_meters_coef, measure_relative_position
-
-# Compute the camera calibration matrix and distortion coefficients given a set of chessboard images
-
-chessboard_images = glob.glob('data/main/camera_cal/*.jpg')
-nx = 9  # chessboard corners in x direction
-ny = 6  # chessboard corners in y direction
-
-calibration_result = calibrate_camera(chessboard_images, (nx, ny))
-ret = calibration_result["ret"]
-mtx = calibration_result["mtx"]
-dist = calibration_result["dist"]
-rvecs = calibration_result["rvecs"]
-tvecs = calibration_result["tvecs"]
+from algorithm.lane_finder import pipeline
+from moviepy.editor import VideoFileClip
 
 image_input_path = 'data/main/test_images/'
 image_output_path = 'data/main/output/images/'
@@ -32,29 +14,18 @@ if not os.path.exists(image_output_path):
 for filename in test_images:
     image_path = os.path.join(image_input_path, filename)
     img = cv2.imread(image_path)
-    # Apply a distortion correction to raw images
-    undistorted_img = cv2.undistort(img, mtx, dist, None, mtx)
-    line_edges = find_line_edges(undistorted_img, yellow_thresh=(140, 255),
-                                 white_thresh=((200, 200, 200), (255, 255, 255)),
-                                 gradient_yellow_threshold=(40, 255), gradient_white_thresh=(40, 255))
-
-    # Bird-view transform
-    warped, M, Minv, src, dst = perspective_transform(line_edges)
-
-    # sliding window algorithm
-    left_fitx, right_fitx, ploty, out_img = fit_polynomial(warped)
-
-    # Calculate curvature and relative position of a car
-    ym_per_pix, xm_per_pix = get_px_to_meters_coef(warped.shape)
-    left_fit, right_fit = fit_polynomial_meters(warped, ym_per_pix, xm_per_pix)
-    left_curverad, right_curverad = measure_curvature_meters(ploty, left_fit, right_fit, ym_per_pix)
-    R = (left_curverad + right_curverad) / 2
-    D = measure_relative_position(warped, xm_per_pix)
-
-    # reverse warp
-    output = reverse_transform(undistorted_img, img, warped, Minv, left_fitx, right_fitx, ploty)
-    cv2.putText(output,
-                f'Radius of Curvature = {R:.2f}(m).Vehicle is {np.absolute(D):.2f}m {"left" if D < 0 else "right"} of '
-                f'center',
-                (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+    output = pipeline(img)
     cv2.imwrite(os.path.join(image_output_path, filename), output)
+
+video_input_path = 'data/main/test_videos/'
+video_output_path = 'data/main/output/videos'
+test_videos = os.listdir(video_input_path)
+
+if not os.path.exists(video_output_path):
+    os.mkdir(video_output_path)
+
+for test_video_name in test_videos:
+    video_path = os.path.join(video_input_path, test_video_name)
+    clip = VideoFileClip(video_path)
+    out_clip = clip.fl_image(pipeline)
+    out_clip.write_videofile(os.path.join(video_output_path, test_video_name), audio=False)
